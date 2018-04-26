3ae15819b702bcb0869b1a3cbb12873b8538fe84
mnava1418 <mnavapena@gmail.com>
2018-04-25 19:16:15 -0500

    folder view controllers

---
+++ Private Notes/UI/FolderViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  FolderViewCell.swift
+//  Private Notes
+//
+//  Created by Martin Nava Pe&a on 25/04/18.
+//  Copyright © 2018 mnava. All rights reserved.
+//
+
+import UIKit
+
+class FolderViewCell: UITableViewCell {
+
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
